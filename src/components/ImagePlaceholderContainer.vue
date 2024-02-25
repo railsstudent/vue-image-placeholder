772607ba5feda8652e26ce924c1de7dc1360b128
Connie Leung <cleung334@gmail.com>
2024-02-25 16:32:17 +0800

    chore: initial commit

---
+++ src/components/ImagePlaceholderContainer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import ImagePlaceholderForm from './ImagePlaceholderForm.vue';
+
+const text = ref('Model');
+const width = ref(400);
+const height = ref(120);
+const color = ref('#fff');
+const backgroundColor = ref('#000');
+
+const url = computed(() => {
+  const w = width.value;
+  const h = height.value;
+  const encText = text.value ? encodeURIComponent(text.value) : `${w} x ${h}`;
+  const encColor = encodeURIComponent(color.value);
+  const encBackground = encodeURIComponent(backgroundColor.value);
+
+  return `https://via.assets.so/img.jpg?w=${w}&h=${h}&tc=${encColor}&bg=${encBackground}&t=${encText}`;
+});
+</script>
+
+<template>
+  <ImagePlaceholderForm
+    v-model:text="text"
+    v-model:width="width"
+    v-model:height="height"
+    v-model:color="color"
+    v-model:backgroundColor="backgroundColor"
+  >
+  </ImagePlaceholderForm>
+  <img :src="url" alt="Image Placeholder" />
+</template>
+
+<style scoped>
+img {
+  margin-top: 1rem;
+}
+</style>
---
+++ src/components/ImagePlaceholderForm.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const text = defineModel('text', { type: String, default: 'Input' });
+const width = defineModel('width', { type: Number, required: true });
+const height = defineModel('height', { type: Number, default: 120 });
+const textColor = defineModel('color', { type: String, default: '#fff' });
+const bgColor = defineModel('backgroundColor', { type: String, default: '#000' });
+</script>
+
+<template>
+  <h3>Redo https://dev.me/products/image-placeholder with defineModel</h3>
+  <div class="container">
+    <div class="field">
+      <label for="text">
+        <span>Text: </span>
+        <input name="text" id="name" v-model="text" />
+      </label>
+    </div>
+    <div class="field">
+      <label for="width">
+        <span>Width: </span>
+        <input name="width" id="width" type="number" v-model="width" />
+      </label>
+    </div>
+    <div class="field">
+      <label for="height">
+        <span>Height: </span>
+        <input name="height" id="height" type="number" v-model="height" />
+      </label>
+    </div>
+    <div class="field">
+      <label for="color">
+        <span>Color: </span>
+        <input name="color" id="color" v-model="textColor" />
+      </label>
+    </div>
+    <div class="field">
+      <label for="backgroundColor">
+        <span>Background Color: </span>
+        <input name="backgroundColor" id="backgroundColor" v-model="bgColor" />
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  margin-bottom: 1rem;
+}
+
+div.container {
+  padding: 0.5rem;
+  border: 1px solid black;
+  border-radius: 12px;
+}
+
+div.field {
+  padding: 0.25rem;
+}
+
+input {
+  padding: 0.5rem;
+  border: 1px solid #4aa;
+  border-radius: 8px;
+}
+</style>
